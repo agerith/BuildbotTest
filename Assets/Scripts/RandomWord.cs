b1ef0c82c4702ab4db398d89417a8143a1b974e1
olivie_j <j.olivier@student.isartdigital.com>
2015-05-27 16:42:28 +0200

    Premiere version

---
+++ Assets/Scripts/RandomWord.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class RandomWord : MonoBehaviour {
+
+	List<string> listWord;
+	public Text text;
+
+	void Start()
+	{
+		listWord = new List<string>();
+		string[] lines = System.IO.File.ReadAllLines(@"D:\MesDocuments\Documents\ISART DIGITAL\GP3_2014-2015\RandomWord\Assets\Words.txt");
+		foreach (string line in lines)
+		{
+			string[] word = line.Split('\t');
+			listWord.Add(word[0]);
+		}
+	}
+
+
+	public void ClickRandom()
+	{
+		int number = Random.Range (0, listWord.Count);
+		text.text = listWord [number];
+	}
+}
